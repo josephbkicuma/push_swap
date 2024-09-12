7faf1e0cf0f357e3f59e9c05361105cd46d64ea3
josephbkicuma <josephbartolomeukicuma@gmail.com>
2024-09-12 13:29:43 +0100

    fix: norminette sort_stack_a file

--- src/sort/sort_stack_a.c
+++ src/sort/sort_stack_a.c
@@ -6,7 +6,7 @@
 /*   By: jquicuma <jquicuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 17:59:47 by jquicuma          #+#    #+#             */
-/*   Updated: 2024/09/11 20:36:28 by jquicuma         ###   ########.fr       */
+/*   Updated: 2024/09/12 13:28:55 by jquicuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,24 +57,24 @@ t_stack	*found_bigger_closest(t_stack **stack_a, t_stack **stack_b)
 	return (target);
 }
 
-void    put_on_top(t_stack **stack_a, t_stack *value)
+void	put_on_top(t_stack **stack_a, t_stack *value)
 {
-    int stack_size;
-    int i;
+	int	stack_size;
+	int	i;
 
-    stack_size = ft_list_size(stack_a);
-    if (value->above_mid)
-    {
-        i = value->index;
-        while (i--)
-            ra(stack_a);   
-    }
-    else
-    {
-        i = stack_size - value->index;
-        while (i--)
-            rra(stack_a);
-    }
+	stack_size = ft_list_size(stack_a);
+	if (value->above_mid)
+	{
+		i = value->index;
+		while (i--)
+			ra(stack_a);
+	}
+	else
+	{
+		i = stack_size - value->index;
+		while (i--)
+			rra(stack_a);
+	}
 }
 
 void	rotate_until_ordenate(t_stack **stack)
@@ -94,19 +94,19 @@ void	rotate_until_ordenate(t_stack **stack)
 	put_on_top(stack, value);
 }
 
-void    push_stack_a_order(t_stack **stack_a, t_stack **stack_b)
+void	push_stack_a_order(t_stack **stack_a, t_stack **stack_b)
 {
-    t_stack *aux_b;
-    t_stack *aux_a;
-    
-    aux_b = *stack_b;
-    while (aux_b)
-    {
-        set_target(stack_a, stack_b);
-        aux_a = found_bigger_closest(stack_a, stack_b);
-        put_on_top(stack_a, aux_a);
-        aux_b = aux_b->next;
-        pa(stack_a, stack_b);
-    }
+	t_stack	*aux_b;
+	t_stack	*aux_a;
+
+	aux_b = *stack_b;
+	while (aux_b)
+	{
+		set_target(stack_a, stack_b);
+		aux_a = found_bigger_closest(stack_a, stack_b);
+		put_on_top(stack_a, aux_a);
+		aux_b = aux_b->next;
+		pa(stack_a, stack_b);
+	}
 	rotate_until_ordenate(stack_a);
 }
