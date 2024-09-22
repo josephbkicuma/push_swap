26b18b5cebb8650e4fe634bbe894cc7e757c840c
josephbkicuma <josephbartolomeukicuma@gmail.com>
2024-09-22 12:08:56 +0100

    feat: scrip de teste

---
+++ teste.sh
@@ -0,0 +1,7 @@
+while true; do 
+    var=($(seq -100000 10000 | shuf -n 100 | tr '\n' ' ')); 
+    clear; 
+    valgrind ./push_swap "${var[@]}" | ./checker_linux "${var[@]}" ; 
+    ./push_swap "${var[@]}" | wc -l;  
+    sleep 1; 
+done
